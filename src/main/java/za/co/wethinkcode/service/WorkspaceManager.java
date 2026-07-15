fd380c167ea6832e018b227a1ecad8dffced0722
nthabies-25 <nthabiekamoloi92@gmail.com>
2026-07-16 00:13:54 +0200

    Workspace Manager class, but incomplete.

--- src/main/java/za/co/wethinkcode/service/WorkspaceManager.java
+++ src/main/java/za/co/wethinkcode/service/WorkspaceManager.java
@@ -10,10 +10,23 @@ import java.util.Collections;
 public class WorkspaceManager {
 
     // TODO: declare private fields
+    private final List<Coworker> coworkers;
+    private final List<Employee> employees;
+    private final LegacyDoorSystem doorSystem;
+    private final String hubName;
 
     // TODO: implement constructor
+    public WorkspaceManager(String hubName) {
+        this.hubName = hubName;
+        this.coworkers = new ArrayList<>();
+        this.employees = new ArrayList<>();
+        this.doorSystem = new LegacyDoorSystem();
+    }
 
     // TODO: implement coworker management methods
+    public void addCoworker(Coworker coworker) {
+        coworkers.add(coworker);
+    }
 
     // TODO: implement employee management methods
 
